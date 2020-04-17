56d3a7da6570fd9b125a38258bc2d1298d3cfc95
skalutki <skalutki@gmail.com>
2020-04-17 22:49:35 +0100

    feat: Version 2.0

--- src/js/main.js
+++ src/js/main.js
@@ -17,82 +17,32 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-const counter = document.querySelector('.form__glass__counter--js');
 
-const currentValue = 0;
-
-const addButton = document.querySelector('.form__button__add--js');
-
-const subtractButton = document.querySelector('.form__button__subtract--js');
-
-
-
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+const value = document.querySelector('counter__value--js');
 const key = new Date().toISOString().slice(0, 10);
 
-
-addButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const currentValue = parseInt(counter.innerHTML) + 1;
-    counter.innerHTML = currentValue;
-    localStorage.setItem(key, currentValue);
-    chechFunction(currentValue);
-    checkCounter(counter.innerHTML);
+if (!localStorage.getItem(key)) {
+    localStorage.setItem(key, 0)
+    value.innerHTML = '0';
+} else {
+    value.innerHTML = localStorage.getItem(key);
+}
 
 
+buttonAdd.addEventListener('click', (e) => {
+
+    localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1)
+    value.innerHTML = localStorage.getItem(key);
 });
 
-subtractButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const currentValue = parseInt(counter.innerHTML) - 1;
-    counter.innerHTML = currentValue;
-    localStorage.setItem(key, currentValue);
-    checkFunction(currentValue);
-    checkCounter(counter.innerHTML);
+buttonRemove.addEventListener('click', (e) => {
 
+    const currentValue = parseInt(localStorage.getItem(key));
+    if (currentValue > 0) {
+        localStorage.setItem(key, localStorage.getItem(key) - 1)
+        value.innerHTML = localStorage.getItem(key);
+    }
 
 });
-
-
-function checkFunction(checkFunction) {
-    if (checkFunction < 0) {
-        alert('Nie możesz zejść ponieżej zera!');
-        counter.innerHTML = currentValue;
-    }
-}
-const localKey = key;
-
-if (counter.innerHTML = 0) {
-    counter.innerHTML = localStorage.getItem(key);
-
-}
-
-console.log(counter.innerHTML);
-console.log(localStorage.getItem(key));
-
-
-
-/*if (checkStatus == 0) {
-    const localKey = localStorage.getItem(key);
-    counter.innerHTML = currentValue;
-}
-
-checkStatus.onstorage = event => {
-    if (event.key != 0) {
-        localKey = localStorage.getItem(key);
-        counter.innerHTML = currentValue;
-    }
-}
-
-if (key ==!0) {
-    localStorage.setItem(key, 0);
-}
-
-if (counter == 0) {
-    localStorage.getItem(key, currentValue);
-    counter.innerHTML = currentValue
-}
-
-if (localStorage(key, currentValue) < 0) {
-    alert("Nie możesz zejść ponieżej zera!");
-    const currentValue = 0;
-}*/
