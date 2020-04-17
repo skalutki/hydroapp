12f4bc6eba0ba0a29cc316919b4b806de7ef23ed
skalutki <skalutki@gmail.com>
2020-04-17 22:06:15 +0100

    feat: Version 1.0

--- src/js/main.js
+++ src/js/main.js
@@ -17,14 +17,82 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-const counter = document.querySelector(".form__glass__counter--js");
+const counter = document.querySelector('.form__glass__counter--js');
 
-const addButton = document.querySelector(".form__button__add--js");
+const currentValue = 0;
 
-const subtractButton = document.querySelector(".form__button__subtract--js");
+const addButton = document.querySelector('.form__button__add--js');
 
-console.log(counter);
+const subtractButton = document.querySelector('.form__button__subtract--js');
 
-console.log(addButton);
 
-console.log(subtractButton);
+
+const key = new Date().toISOString().slice(0, 10);
+
+
+addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentValue = parseInt(counter.innerHTML) + 1;
+    counter.innerHTML = currentValue;
+    localStorage.setItem(key, currentValue);
+    chechFunction(currentValue);
+    checkCounter(counter.innerHTML);
+
+
+});
+
+subtractButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentValue = parseInt(counter.innerHTML) - 1;
+    counter.innerHTML = currentValue;
+    localStorage.setItem(key, currentValue);
+    checkFunction(currentValue);
+    checkCounter(counter.innerHTML);
+
+
+});
+
+
+function checkFunction(checkFunction) {
+    if (checkFunction < 0) {
+        alert('Nie możesz zejść ponieżej zera!');
+        counter.innerHTML = currentValue;
+    }
+}
+const localKey = key;
+
+if (counter.innerHTML = 0) {
+    counter.innerHTML = localStorage.getItem(key);
+
+}
+
+console.log(counter.innerHTML);
+console.log(localStorage.getItem(key));
+
+
+
+/*if (checkStatus == 0) {
+    const localKey = localStorage.getItem(key);
+    counter.innerHTML = currentValue;
+}
+
+checkStatus.onstorage = event => {
+    if (event.key != 0) {
+        localKey = localStorage.getItem(key);
+        counter.innerHTML = currentValue;
+    }
+}
+
+if (key ==!0) {
+    localStorage.setItem(key, 0);
+}
+
+if (counter == 0) {
+    localStorage.getItem(key, currentValue);
+    counter.innerHTML = currentValue
+}
+
+if (localStorage(key, currentValue) < 0) {
+    alert("Nie możesz zejść ponieżej zera!");
+    const currentValue = 0;
+}*/
